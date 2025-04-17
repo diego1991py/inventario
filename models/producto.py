1a8f5eac08891ee399be3258a07a7db7c52e6cfe
Diego <91valenciadiego@gmail.com>
2025-04-17 11:44:07 -0500

    Restructuración completa de la aplicación

--- models/producto.py
+++ models/producto.py
@@ -11,20 +11,3 @@
         to_dict : convertir a diccionario para enviar a csv"""
 
 
-class Producto:
-    def __init__(self, id, nombre, precio, cantidad):
-        self.id = id
-        self.nombre = nombre
-        self.precio = precio
-        self.cantidad = cantidad
-
-    def __str__(self):
-        return f"{self.id} - {self.nombre} - {self.precio} - {self.cantidad}"
-    
-    def to_dict(self):
-        return{
-            "id " : self.id,
-            "nombre" : self.nombre,
-            "precio" : self.precio,
-            "cantidad" : self.cantidad
-        }
