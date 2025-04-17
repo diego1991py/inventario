4ca3b5c79db76670d79ad4d4df0d8d2901c4a9d5
Diego <91valenciadiego@gmail.com>
2025-04-17 14:50:07 -0500

    Estructura de la Clase Producto

--- models/producto.py
+++ models/producto.py
@@ -11,3 +11,28 @@
         to_dict : convertir a diccionario para enviar a csv"""
 
 
+class Producto:
+    def __init__(self, id, nombre, precio, cantidad):
+        self.id = id
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+
+    def __str__(self):
+        return f"id = {self.id}, nombre = {self.nombre}, precio = ${self.precio}, cantidad = {self.cantidad}"
+    
+    def to_dict(self):
+        return{
+            "id" : self.id,
+            "nombre" : self.nombre,
+            "precio" : self.precio,
+            "cantidad" : self.cantidad
+        }
+    
+
+"""nuevo_producto = Producto(1,"CPU",500000,10)
+
+print(nuevo_producto.to_dict())
+print(nuevo_producto.__str__())"""
+
+
